cbcccad3f503279581e7af587968b8e9741a7180
neblob <neblob_@msn.com>
2015-07-15 11:14:44 +0700

    2nd code

--- app/src/main/java/com/example/toshiba/hctmmmmapplication/MainActivity.java
+++ app/src/main/java/com/example/toshiba/hctmmmmapplication/MainActivity.java
@@ -1,37 +1,28 @@
 package com.example.toshiba.hctmmmmapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static final int TAKE_PICTURE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonTest = (Button) findViewById(R.id.button_cameramain);
+        buttonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/com/example/toshiba/hctmmmmapplication/MySurfaceView.java
@@ -0,0 +1,140 @@
+package com.example.toshiba.hctmmmmapplication;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.util.AttributeSet;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class MySurfaceView extends SurfaceView
+        implements SurfaceHolder.Callback {
+    private static Context mContext;
+    private static Camera mCamera;
+
+    public MySurfaceView(Context context) {
+        this(context, null);
+    }
+
+    public MySurfaceView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    @SuppressWarnings("deprecation")
+    public MySurfaceView(Context context, AttributeSet attrs,
+                         int defaultsyStyle) {
+        super(context, attrs, defaultsyStyle);
+        mContext = context;
+
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(this);
+
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera = Camera.open();
+            mCamera.setPreviewDisplay(holder);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        Camera.Parameters params = mCamera.getParameters();
+        List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+        Camera.Size selected = sizes.get(0);
+        params.setPreviewSize(selected.width, selected.height);
+        params.setFlashMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        mCamera.setParameters(params);
+
+        setmCameraDisplayOrientation();
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mCamera.stopPreview();
+        mCamera.release();
+    }
+
+    public void setmCameraDisplayOrientation() {
+        Camera.CameraInfo info =
+                new Camera.CameraInfo();
+        Camera.getCameraInfo(0, info);
+
+        int rotation = ((WindowManager) mContext
+                .getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                .getRotation();
+
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;
+        } else {
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        mCamera.setDisplayOrientation(result);
+    }
+
+    private static class TakePictureCallback
+            implements Camera.PictureCallback {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            try {
+                File imageFile = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+                FileOutputStream fos = new FileOutputStream(imageFile);
+                fos.write(data);
+                fos.flush();
+                fos.close();
+
+                Toast.makeText(mContext, "Picture saved.",
+                        Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public static void takePicture() {
+        Camera.Parameters params = mCamera.getParameters();
+        List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+        Camera.Size selected = sizes.get(0);
+        params.setPictureSize(selected.width, selected.height);
+        mCamera.setParameters(params);
+
+        TakePictureCallback callback = new TakePictureCallback();
+        mCamera.takePicture(null, null, null, callback);
+    }
+}
